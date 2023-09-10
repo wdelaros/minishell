11e2e9c7c260f2be598b07cba2a10db766c1d91d
Raphael Pelchat <rapelcha@Raphaels-MacBook-Pro.local>
2023-09-10 17:23:41 -0400

    erreur de merde regleer

--- src/parsing/var_handler.c
+++ src/parsing/var_handler.c
@@ -40,8 +40,6 @@ static char	*clean_var(char *str, int k)
 	len = 0;
 	while (str[--k] && str[k] != '$')
 		len++;
-	//printf ("STRING DE LA PATANTE: %s\n", str);
-	//printf ("TRUC DE LA PATANTE: %d\n", len);
 	res = ft_calloc((ft_strlen(str) - len + 1) + 1, sizeof(char));
 	ft_strlcpy(res, str, k + 1);
 	k += len + 1;
@@ -56,15 +54,13 @@ static char	*wagadoo_machine_3(char *str, int start, char *var)
 
 	res = ft_calloc(start, sizeof(char));
 	ft_strlcpy(res, str, start + 1);
-	printf ("PREMIER CPY: %s\n", res);
 	res = ft_fstrjoin(res, var);
-	printf ("DEUXIEME CPY: %s\n", res);
-	if (str[start])
+	if (str[start] == '$')
 		start++;
-	while (str[start] && str[start] != '$')
+	while (str[start] && (str[start] != '$'
+			&& str[start] != DOUBLE_QUOTE && str[start] != SINGLE_QUOTE))
 		start++;
 	res = ft_fstrjoin(res, &str[start]);
-	printf ("TROISIEME CPY: %s\n", res);
 	ft_xfree(str);
 	return (res);
 }
@@ -87,13 +83,10 @@ static int	normal_condition(char **input, char **env, int i)
 		var[j++] = temp[i++];
 	var_temp = ft_fstrjoin(var, "=");
 	var = ft_strdup(get_var_parsing(var_temp, env));
-	//printf ("UWU: %d\n", i);
-	//printf ("SADASDASDASDASD: %s	%d\n", *input, i);
 	if (var == NULL)
 		*input = clean_var(*input, i);
 	else
 		*input = wagadoo_machine_3(*input, start, var);
-	//printf ("%s\n", *input);
 	ft_xfree(var_temp);
 	ft_xfree(temp);
 	return (0);
