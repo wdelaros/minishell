53200a8137a8b5b201aa01d32d18ca27f23aaa96
wdelaros <williamdelarosbil@gmail.com>
2023-10-01 13:21:17 -0400

    exit code fix

--- src/execution/ex_utils.c
+++ src/execution/ex_utils.c
@@ -6,7 +6,7 @@
 /*   By: wdelaros <wdelaros@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 13:34:50 by wdelaros          #+#    #+#             */
-/*   Updated: 2023/09/19 13:34:51 by wdelaros         ###   ########.fr       */
+/*   Updated: 2023/10/01 13:16:42 by wdelaros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,11 @@ void	ft_exit_message(char **fcmd, t_cmd *lcmd, char ***cmd, int code)
 		ft_dprintf(2, "minishell: %s: command not found\n", fcmd[0]);
 	else if (code == 126)
 		ft_dprintf(2, "minishell: %s: Permission denied\n", fcmd[0]);
+	else if (code == 1261)
+	{
+		ft_dprintf(2, "minishell: %s: is a directory\n", fcmd[0]);
+		code = 126;
+	}
 	else
 		ft_dprintf(2, "minishell: %s: operations not permitted\n", fcmd[0]);
 	ft_free_all_pipe(lcmd, cmd);
@@ -42,14 +47,19 @@ void	ft_exit_message(char **fcmd, t_cmd *lcmd, char ***cmd, int code)
 */
 void	exec(char **fcmd, t_cmd	*lcmd, char ***cmd_to_free)
 {
+	struct stat path;
+	
 	struc()->path = findpath(struc());
 	find_executable(fcmd, 0, struc());
 	if (execve(struc()->cmdpath, fcmd, struc()->envp) == -1)
 	{
+		lstat(struc()->cmdpath, &path);
 		if (access(struc()->cmdpath, F_OK))
 			ft_exit_message(fcmd, lcmd, cmd_to_free, 127);
 		else if (access(struc()->cmdpath, X_OK))
 			ft_exit_message(fcmd, lcmd, cmd_to_free, 126);
+		else if (S_ISDIR(path.st_mode))
+			ft_exit_message(fcmd, lcmd, cmd_to_free, 1261);
 		else
 			ft_exit_message(fcmd, lcmd, cmd_to_free, 1);
 	}
